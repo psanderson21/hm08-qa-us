5881d18b2337f222268dda2e3217817e5c3efca2
Patrick Sanderson <161405278+psanderson21@users.noreply.github.com>
2024-03-23 17:29:56 -0500

    second commit - excl 'node_modules' folder

---
+++ wdio.conf.js
@@ -0,0 +1,48 @@
+exports.config = {
+    runner: 'local',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    headless: true,
+    capabilities: [
+        {
+        maxInstances: 5,
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
+    },
+    /*  NOTE: Firefox commented out at tutor's suggestion.  See README.md for details
+    {
+        maxInstances: 5,
+        browserName: 'firefox',
+        acceptInsecureCerts: true,
+        'moz:firefoxOptions': {
+            args: ['-headless']
+        }
+    }
+    */ 
+],
+    logLevel: 'error',
+    bail: 0,
+    baseUrl: 'https://cnt-370ed386-1f5c-4bc2-8f58-67d51510fe4e.containerhub.tripleten-services.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [
+        'chromedriver', 
+        'geckodriver', 
+        'intercept', 
+    ],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
